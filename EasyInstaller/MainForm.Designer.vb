75b9e4dbebb3c3c403633baa06ec7194fd52c9d9
Steve Stepp <steppsr@gmail.com>
2011-03-27 18:45:21 -0500

    Begin project in Git

---
+++ EasyInstaller/MainForm.Designer.vb
@@ -0,0 +1,154 @@
+﻿'
+' Created by SharpDevelop.
+' User: steppsr
+' Date: 3/11/2010
+' Time: 11:23 AM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Partial Class MainForm
+	Inherits System.Windows.Forms.Form
+	
+	''' <summary>
+	''' Designer variable used to keep track of non-visual components.
+	''' </summary>
+	Private components As System.ComponentModel.IContainer
+	
+	''' <summary>
+	''' Disposes resources used by the form.
+	''' </summary>
+	''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		If disposing Then
+			If components IsNot Nothing Then
+				components.Dispose()
+			End If
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+	
+	''' <summary>
+	''' This method is required for Windows Forms designer support.
+	''' Do not change the method contents inside the source code editor. The Forms designer might
+	''' not be able to load this method if it was changed manually.
+	''' </summary>
+	Private Sub InitializeComponent()
+		Me.pictureBox1 = New System.Windows.Forms.PictureBox
+		Me.pictureBox2 = New System.Windows.Forms.PictureBox
+		Me.button1 = New System.Windows.Forms.Button
+		Me.button2 = New System.Windows.Forms.Button
+		Me.button3 = New System.Windows.Forms.Button
+		Me.button4 = New System.Windows.Forms.Button
+		Me.button5 = New System.Windows.Forms.Button
+		Me.webBrowser1 = New System.Windows.Forms.WebBrowser
+		CType(Me.pictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
+		CType(Me.pictureBox2,System.ComponentModel.ISupportInitialize).BeginInit
+		Me.SuspendLayout
+		'
+		'pictureBox1
+		'
+		Me.pictureBox1.Location = New System.Drawing.Point(442, 8)
+		Me.pictureBox1.Name = "pictureBox1"
+		Me.pictureBox1.Size = New System.Drawing.Size(144, 148)
+		Me.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+		Me.pictureBox1.TabIndex = 0
+		Me.pictureBox1.TabStop = false
+		'
+		'pictureBox2
+		'
+		Me.pictureBox2.Location = New System.Drawing.Point(8, 8)
+		Me.pictureBox2.Name = "pictureBox2"
+		Me.pictureBox2.Size = New System.Drawing.Size(428, 148)
+		Me.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+		Me.pictureBox2.TabIndex = 1
+		Me.pictureBox2.TabStop = false
+		'
+		'button1
+		'
+		Me.button1.Enabled = false
+		Me.button1.Location = New System.Drawing.Point(442, 162)
+		Me.button1.Name = "button1"
+		Me.button1.Size = New System.Drawing.Size(144, 40)
+		Me.button1.TabIndex = 2
+		Me.button1.UseVisualStyleBackColor = true
+		AddHandler Me.button1.Click, AddressOf Me.Button1Click
+		'
+		'button2
+		'
+		Me.button2.Enabled = false
+		Me.button2.Location = New System.Drawing.Point(442, 208)
+		Me.button2.Name = "button2"
+		Me.button2.Size = New System.Drawing.Size(144, 40)
+		Me.button2.TabIndex = 3
+		Me.button2.UseVisualStyleBackColor = true
+		AddHandler Me.button2.Click, AddressOf Me.Button2Click
+		'
+		'button3
+		'
+		Me.button3.Enabled = false
+		Me.button3.Location = New System.Drawing.Point(442, 254)
+		Me.button3.Name = "button3"
+		Me.button3.Size = New System.Drawing.Size(144, 40)
+		Me.button3.TabIndex = 4
+		Me.button3.UseVisualStyleBackColor = true
+		AddHandler Me.button3.Click, AddressOf Me.Button3Click
+		'
+		'button4
+		'
+		Me.button4.Enabled = false
+		Me.button4.Location = New System.Drawing.Point(442, 300)
+		Me.button4.Name = "button4"
+		Me.button4.Size = New System.Drawing.Size(144, 40)
+		Me.button4.TabIndex = 5
+		Me.button4.UseVisualStyleBackColor = true
+		AddHandler Me.button4.Click, AddressOf Me.Button4Click
+		'
+		'button5
+		'
+		Me.button5.Location = New System.Drawing.Point(442, 346)
+		Me.button5.Name = "button5"
+		Me.button5.Size = New System.Drawing.Size(144, 40)
+		Me.button5.TabIndex = 6
+		Me.button5.Text = "Close"
+		Me.button5.UseVisualStyleBackColor = true
+		AddHandler Me.button5.Click, AddressOf Me.Button5Click
+		'
+		'webBrowser1
+		'
+		Me.webBrowser1.Location = New System.Drawing.Point(8, 162)
+		Me.webBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+		Me.webBrowser1.Name = "webBrowser1"
+		Me.webBrowser1.ScrollBarsEnabled = false
+		Me.webBrowser1.Size = New System.Drawing.Size(428, 222)
+		Me.webBrowser1.TabIndex = 7
+		'
+		'MainForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.White
+		Me.ClientSize = New System.Drawing.Size(598, 396)
+		Me.Controls.Add(Me.webBrowser1)
+		Me.Controls.Add(Me.button5)
+		Me.Controls.Add(Me.button4)
+		Me.Controls.Add(Me.button3)
+		Me.Controls.Add(Me.button2)
+		Me.Controls.Add(Me.button1)
+		Me.Controls.Add(Me.pictureBox2)
+		Me.Controls.Add(Me.pictureBox1)
+		Me.Name = "MainForm"
+		Me.Text = "EasyInstaller"
+		AddHandler Load, AddressOf Me.MainFormLoad
+		CType(Me.pictureBox1,System.ComponentModel.ISupportInitialize).EndInit
+		CType(Me.pictureBox2,System.ComponentModel.ISupportInitialize).EndInit
+		Me.ResumeLayout(false)
+	End Sub
+	Private webBrowser1 As System.Windows.Forms.WebBrowser
+	Private button5 As System.Windows.Forms.Button
+	Private button4 As System.Windows.Forms.Button
+	Private button3 As System.Windows.Forms.Button
+	Private button2 As System.Windows.Forms.Button
+	Private button1 As System.Windows.Forms.Button
+	Private pictureBox2 As System.Windows.Forms.PictureBox
+	Private pictureBox1 As System.Windows.Forms.PictureBox
+End Class
---
+++ EasyInstaller/MainForm.vb
@@ -0,0 +1,287 @@
+﻿'
+' Created by SharpDevelop.
+' User: steppsr
+' Date: 3/11/2010
+' Time: 11:23 AM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+
+Public Partial Class MainForm
+
+	Dim sButton1Folder As String = ""
+	Dim sButton2Folder As String = ""
+	Dim sButton3Folder As String = ""
+	Dim sButton4Folder As String = ""
+	Dim sButton1File As String = ""
+	Dim sButton2File As String = ""
+	Dim sButton3File As String = ""
+	Dim sButton4File As String = ""
+	Dim sButton1Title As String = ""
+	Dim sButton2Title As String = ""
+	Dim sButton3Title As String = ""
+	Dim sButton4Title As String = ""
+	Dim sButton1PrereqFolder As String = ""
+	Dim sButton2PrereqFolder As String = ""
+	Dim sButton3PrereqFolder As String = ""
+	Dim sButton4PrereqFolder As String = ""
+	Dim sButton1PrereqFile As String = ""
+	Dim sButton2PrereqFile As String = ""
+	Dim sButton3PrereqFile As String = ""
+	Dim sButton4PrereqFile As String = ""
+	Dim sButton1PrereqInstallFolder As String = ""
+	Dim sButton2PrereqInstallFolder As String = ""
+	Dim sButton3PrereqInstallFolder As String = ""
+	Dim sButton4PrereqInstallFolder As String = ""
+	Dim sButton1PrereqInstallFile As String = ""
+	Dim sButton2PrereqInstallFile As String = ""
+	Dim sButton3PrereqInstallFile As String = ""
+	Dim sButton4PrereqInstallFile As String = ""
+	
+	Dim COMPANY_NAME As String = ""
+	Dim APP_NAME As String = ""
+
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+	
+	Sub MainFormLoad(sender As Object, e As EventArgs)
+		
+		' Load application settings by reading in the main.html file under the EasyInstaller folder
+		LoadMainHTML()
+		
+	End Sub
+	
+	Sub LoadMainHTML()
+		
+		'********** LOAD APPLICATION SETTINGS ******************
+		
+		Dim sHTML As String = ""
+		
+		Try
+			'---Import an HTML file that will load a summary for the tool
+			Dim sReader As System.IO.StreamReader
+			Dim sTemp As String = ""
+			sReader = New System.IO.StreamReader("easyinstaller/main.html")
+			While(sReader.Peek > -1)
+				sTemp = sReader.ReadLine
+				
+				' Check for config settings in the comments of the main.html file and store in some global vars
+				If instr(sTemp,"Button1_Folder") > 0 Then
+					sButton1Folder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button2_Folder") > 0 Then
+					sButton2Folder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button3_Folder") > 0 Then
+					sButton3Folder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button4_Folder") > 0 Then
+					sButton4Folder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button1_File") > 0 Then
+					sButton1File = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button2_File") > 0 Then
+					sButton2File = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button3_File") > 0 Then
+					sButton3File = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button4_File") > 0 Then
+					sButton4File = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button1_Title") > 0 Then
+					sButton1Title = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button2_Title") > 0 Then
+					sButton2Title = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button3_Title") > 0 Then
+					sButton3Title = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button4_Title") > 0 Then
+					sButton4Title = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"COMPANY_NAME") > 0 Then
+					COMPANY_NAME = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"APP_NAME") > 0 Then
+					APP_NAME = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button1_Prereq_Folder") > 0 Then
+					sButton1PrereqFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button2_Prereq_Folder") > 0 Then
+					sButton2PrereqFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button3_Prereq_Folder") > 0 Then
+					sButton3PrereqFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button4_Prereq_Folder") > 0 Then
+					sButton4PrereqFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button1_Prereq_File") > 0 Then
+					sButton1PrereqFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button2_Prereq_File") > 0 Then
+					sButton2PrereqFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button3_Prereq_File") > 0 Then
+					sButton3PrereqFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button4_Prereq_File") > 0 Then
+					sButton4PrereqFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button1_Prereq_Install_File") > 0 Then
+					sButton1PrereqInstallFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button2_Prereq_Install_File") > 0 Then
+					sButton2PrereqInstallFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button3_Prereq_Install_File") > 0 Then
+					sButton3PrereqInstallFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button4_Prereq_Install_File") > 0 Then
+					sButton4PrereqInstallFile = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button1_Prereq_Install_Folder") > 0 Then
+					sButton1PrereqInstallFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button2_Prereq_Install_Folder") > 0 Then
+					sButton2PrereqInstallFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button3_Prereq_Install_Folder") > 0 Then
+					sButton3PrereqInstallFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+				If instr(sTemp,"Button4_Prereq_Install_Folder") > 0 Then
+					sButton4PrereqInstallFolder = mid(sTemp,instr(sTemp,"=")+1)
+				End If
+
+				' Append regular HTML into var for main application message
+				sHTML = sHTML & sTemp
+				
+			End While
+			
+			sReader.Close
+			sReader.Dispose
+			
+		Catch
+			' If there was an error, make sure the string is empty
+			sHTML = ""
+		End Try
+		
+		pictureBox1.ImageLocation = "easyinstaller/company_logo.png"
+		pictureBox2.ImageLocation = "easyinstaller/app_banner.png"
+
+		' The application main message is in a webBrowser we will need to give it some HTML and refresh
+		webBrowser1.DocumentText =	sHTML
+		webBrowser1.Refresh
+		
+		' Using config settings set title for buttons and make them enabled
+		If sButton1Title <> "" Then
+			button1.Text = sButton1Title
+			button1.Enabled = True
+		End If
+		If sButton2Title <> "" Then
+			button2.Text = sButton2Title
+			button2.Enabled = True
+		End If
+		If sButton3Title <> "" Then
+			button3.Text = sButton3Title
+			button3.Enabled = True
+		End If
+		If sButton4Title <> "" Then
+			button4.Text = sButton4Title
+			button4.Enabled = True
+		End If
+				
+	End Sub
+	
+	Sub RunButtonProcess(sButtonFolder As String, sButtonFile As String, sButtonPrereqFolder As String, sButtonPrereqFile As String, sButtonPrereqInstallFolder As String, sButtonPrereqInstallFile As String)
+		
+		Try
+			' ***************** BUTTON PRESSED ***************
+			'
+			' Create an external process to run the file associated with each button. Wait for the process
+			' to complete before returning.
+			'
+			Dim myProcess As Process
+			If sButtonFile <> "" Then
+				
+				' Lets check the prerequisites first
+				Dim sPreq As String = ""
+				If sButtonPrereqFile <> "" Then		'filename cant be empty
+					If sButtonPrereqFolder <> "" Then
+						sPreq = sPreq & sButtonPrereqFolder & "\"
+					End If
+					sPreq = sPreq & sButtonPrereqFile
+					If System.IO.File.Exists(sPreq) = False Then	'does the require file exist? If not then run prereq install
+						If sButtonPrereqInstallFolder <> "" and sButtonPrereqInstallFile <> "" Then
+							myProcess = System.Diagnostics.Process.Start(sButtonPrereqInstallFolder & "\" & sButtonPrereqInstallFile)
+							myProcess.WaitForExit()
+							myProcess.Close()
+						Else
+							msgbox("Missing path/filename for prerequisite install.", vbOKOnly, "ERROR")
+						End If
+					Else
+						msgbox("Prerequisite already installed.",vbOkOnly,"NOTE")
+					End If
+				Else
+					'msgbox("No prerequisites listed.",vbOKOnly,"DEBUG")
+				End If
+				
+				' And now for the main app for the button
+			    If sButtonFolder <> "" Then
+			    	myProcess = System.Diagnostics.Process.Start(sButtonFolder & "\" & sButtonFile)
+				Else
+				    myProcess = System.Diagnostics.Process.Start(sButtonFile)
+			    End If
+			    myProcess.WaitForExit()
+			    myProcess.Close()
+			Else
+				msgbox("There appears to be a problem with this option. Please notify " & COMPANY_NAME & " support staff for help.",vbOKOnly,"Easy Install Error")
+			End If
+		Catch
+			msgbox("There appears to be a problem with this option. Please notify " & COMPANY_NAME & " support staff for help.",vbOkOnly,"Easy Install Error")
+		End Try
+		
+
+	End Sub
+	
+	Sub Button1Click(sender As Object, e As EventArgs)
+		
+		RunButtonProcess(sButton1Folder, sButton1File, sButton1PrereqFolder, sButton1PrereqFile, sButton1PrereqInstallFolder, sButton1PrereqInstallFile)
+		
+	End Sub
+	
+	Sub Button2Click(sender As Object, e As EventArgs)
+		
+		RunButtonProcess(sButton2Folder, sButton2File, sButton2PrereqFolder, sButton2PrereqFile, sButton2PrereqInstallFolder, sButton2PrereqInstallFile)
+		
+	End Sub
+	
+	Sub Button3Click(sender As Object, e As EventArgs)
+		
+		RunButtonProcess(sButton3Folder, sButton3File, sButton3PrereqFolder, sButton3PrereqFile, sButton3PrereqInstallFolder, sButton3PrereqInstallFile)
+		
+	End Sub
+	
+	Sub Button4Click(sender As Object, e As EventArgs)
+		
+		RunButtonProcess(sButton4Folder, sButton4File, sButton4PrereqFolder, sButton4PrereqFile, sButton4PrereqInstallFolder, sButton4PrereqInstallFile)
+		
+	End Sub
+
+	Sub Button5Click(sender As Object, e As EventArgs)
+		
+		End
+		
+	End Sub
+	
+End Class
